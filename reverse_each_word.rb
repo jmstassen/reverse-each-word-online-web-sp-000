b8ab03b0d8173f603d2ba19fcdb1dd6dd0350993
John Stassen <john.stassen@gmail.com>
2020-08-21 22:08:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   phrase_array = string.split(/ /)
-  phrase_array.each_with_index do |word, index|
-    phrase_array[index] = word.reverse
+  phrase_array.collect do |word|
+    word.reverse
   end
-  phrase_array.join(" ")
 end
